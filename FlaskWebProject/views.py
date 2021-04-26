051b0a5edd9c860862f8fddef009811c54a0cf8c
ccs3516 <csaltosax@hotmail.com>
2021-04-27 00:18:02 +0800

    add https to redirect_uri

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -90,7 +90,7 @@ def authorized():
         result = _build_msal_app(cache).acquire_token_by_authorization_code(
         request.args['code'],
         scopes=Config.SCOPE,  # Misspelled scope would cause an HTTP 400 error here
-        redirect_uri=url_for("authorized", _external=True))
+        redirect_uri=url_for("authorized", _external=True, _scheme="https"))
 
         if "error" in result:
             return render_template("auth_error.html", result=result)
